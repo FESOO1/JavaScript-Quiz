aa5e7dab7f3a54fc09faacb117c49319c00c5166
FES001 <firdavs7794948@gmail.com>
2025-01-08 20:44:31 +0500

    Working on it,

---
+++ scripts/display-questions.js
@@ -0,0 +1,5 @@
+const questionsThemselves = document.querySelectorAll('.quiz-itself-form-question-itself');
+const answersChoices = document.querySelectorAll('.quiz-itself-form-answer-itself');
+const choicesInputs = document.querySelectorAll('.quiz-itself-form-answer-input');
+
+// DISPLAY THE QUESTIONS AND THE CHOICES
